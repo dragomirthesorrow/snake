5e2eef45fd1ea571be1978232b6c24ff452d4aff
dragomirthesorrow <dragomir-onishhenko@yandex.ru>
2015-11-27 18:06:41 +0300

    snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,31 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
-
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
-
-
+            Console.SetBufferSize(80, 25);
+            HorizontalLine upline = new HorizontalLine(0,78,0,'+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftvline = new VerticalLine(0,24,0,'+');
+            VerticalLine rightvline = new VerticalLine(0, 24, 78, '+');
+            rightvline.Draw();
+            upline.Draw();
+            downline.Draw();
+            leftvline.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
